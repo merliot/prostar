aff59d2055e609d6521663f210d40774d02cdf7e
Scott Feldman <sfeldma@gmail.com>
2024-04-19 15:01:07 -0700

    add x86-64 target

---
+++ ps30m-x86-64.go
@@ -0,0 +1,46 @@
+//go:build x86_64
+
+package ps30m
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/goburrow/serial"
+)
+
+type transport struct {
+	serial.Port
+}
+
+var dev = "/dev/ttyUSB0"
+
+func newTransport() *transport {
+	port, err := serial.Open(&serial.Config{
+		Address:  dev,
+		BaudRate: 9600,
+		StopBits: 2,
+		Parity:   "N",
+		Timeout:  time.Second,
+	})
+	if err != nil {
+		fmt.Println("Error opening serial port", dev, err)
+		return nil
+	}
+	return &transport{port}
+}
+
+func (t *transport) Read(buf []byte) (n int, err error) {
+	if t.Port == nil {
+		return 0, errors.New("Port" + dev + "not opened")
+	}
+	return t.Port.Read(buf)
+}
+
+func (t *transport) Write(buf []byte) (n int, err error) {
+	if t.Port == nil {
+		return 0, errors.New("Port " + dev + "not opened")
+	}
+	return t.Port.Write(buf)
+}
