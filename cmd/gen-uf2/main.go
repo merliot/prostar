f94e03eb1a25e76e4171a8e06bc394455590311d
Scott Feldman <sfeldma@gmail.com>
2024-04-30 15:16:56 -0700

    use /uf2s dir

--- cmd/gen-uf2/main.go
+++ cmd/gen-uf2/main.go
@@ -10,7 +10,7 @@ import (
 //go:generate go run main.go
 func main() {
 	prostar := prostar.New("proto", "prostar", "proto").(*prostar.Prostar)
-	if err := prostar.GenerateUf2s("../.."); err != nil {
+	if err := prostar.GenerateUf2s("../../uf2s/"); err != nil {
 		log.Println("Error generating UF2s:", err)
 		os.Exit(1)
 	}
